fa78cb4254510deb0e3ee2fab1a8d4d0d637fadb
YJE888 <75539276+YJE888@users.noreply.github.com>
2022-01-17 11:30:30 +0900

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -9,23 +9,23 @@ systemctl stop firewalld
 systemctl disable firewalld
 echo -e "\n"
 
-echo -e "SETUP HOSTFILE"
-echo -n "Enter the new hostname: "
-read HOST
-echo -n "Enter the new host's IP: "
-read IP
-cat <<EOF >> /etc/hosts
-192.168.137.199 mon1
-192.168.137.200 mon2
-192.168.137.201 osd-1
-192.168.137.202 osd-2
-192.168.137.203 osd-3
-192.168.137.204 osd-4
-192.168.137.198 mon3
-192.168.137.205 osd-5
-$IP     $HOST
-EOF
-echo -e "\n"
+#echo -e "SETUP HOSTFILE"
+#echo -n "Enter the new hostname: "
+#read HOST
+#echo -n "Enter the new host's IP: "
+#read IP
+#cat <<EOF >> /etc/hosts
+#192.168.137.199 mon1
+#192.168.137.200 mon2
+#192.168.137.201 osd-1
+#192.168.137.202 osd-2
+#192.168.137.203 osd-3
+#192.168.137.204 osd-4
+#192.168.137.198 mon3
+#192.168.137.205 osd-5
+#$IP     $HOST
+#EOF
+#echo -e "\n"
 
 echo -e "INSTALL PYTHON3"
 yum -y install python3
